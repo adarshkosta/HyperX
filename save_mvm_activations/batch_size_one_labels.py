b1461b3f1fc1199905098a6fb13cf96f247ed719
Adarsh K Kosta <akosta@ecegrid-thin3.ecn.purdue.edu>
2020-09-27 09:29:08 -0400

    Fix multi-gpu register-hook v1

--- save_mvm_activations/batch_size_one_labels.py
+++ save_mvm_activations/batch_size_one_labels.py
@@ -3,19 +3,23 @@ import torch
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-b_train', default=1024, type=int,
-                     metavar='N', help='mini-batch size (default: 1024)')
-parser.add_argument('-b_test', default=1024, type=int,
-                     metavar='N', help='mini-batch size (default: 1024)')
+parser.add_argument('-b_train', default=1000, type=int,
+                     metavar='N', help='mini-batch size (default: 1000)')
+parser.add_argument('-b_test', default=1000, type=int,
+                     metavar='N', help='mini-batch size (default: 1000)')
 parser.add_argument('--dataset', metavar='DATASET', default='cifar10', help='dataset name or folder')
 parser.add_argument('--model', '-a', metavar='MODEL', default='resnet20', help='name of the model')
 args = parser.parse_args()
+
+trainsize = 50000
+testsize = 10000
 
 ################ SAVE THE LABELS (we do it only once)
 base_src_path = '/home/nano01/a/esoufler/activations/multiple_batches/'+str(args.dataset)+'/'+str(args.model)+'/'+'train/labels/'
 base_dst_path = '/home/nano01/a/esoufler/activations/one_batch/'+str(args.dataset)+'/'+str(args.model)+'/'+'train/labels/'
 batch_size = args.b_train
-num = int(50000/batch_size)+1
+
+num = int(trainsize/batch_size)+1
 
 for batch in range(num):
     print(batch)
@@ -28,11 +32,12 @@ for batch in range(num):
         dst_filename = base_dst_path + 'labels_' + str(batch_size * batch + element_in_index) + '.pth.tar' 
         torch.save(dst_tensor, dst_filename)
         element_in_index += 1
+        
 
 base_src_path = '/home/nano01/a/esoufler/activations/multiple_batches/'+str(args.dataset)+'/'+str(args.model)+'/'+'test/labels/'
 base_dst_path = '/home/nano01/a/esoufler/activations/one_batch/'+str(args.dataset)+'/'+str(args.model)+'/'+'test/labels/'
 batch_size = args.b_test
-num = int(10000/batch_size)+1
+num = int(testsize/batch_size)+1
 
 for batch in range(num):
     print(batch)
--- save_mvm_activations/save_mvm_resnet20.py
+++ save_mvm_activations/save_mvm_resnet20.py
@@ -3,11 +3,12 @@
 """
 Created on Fri Sep 25 22:42:24 2020
 
-@author: akosta (original by Shubham Negi)
+@author: akosta 
 """
 import os
 import sys
 import time
+import collections
 
 #Min
 #savedir = '/home/min/a/akosta/Current_Projects/Hybrid_RRAM-SRAM/activations/multiple_batches/'
@@ -16,7 +17,7 @@ import time
 #savedir= '/home/nano01/a/esoufler/activations/multiple_batches/'
 
 #Filepath handling
-root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+root_dir = os.path.dirname(os.getcwd())
 inference_dir = os.path.join(root_dir, "inference")
 src_dir = os.path.join(root_dir, "src")
 models_dir = os.path.join(root_dir, "models")
@@ -79,6 +80,7 @@ for path, dirs, files in os.walk(models_dir):
     break # only traverse top level directory
 model_names.sort()
 
+#%%
 # Evaluate on a model
 def test(device):
     global best_acc
@@ -118,84 +120,80 @@ def test(device):
   
 # Intermediate feature maps
 activation = {}
-def get_activation(name):
-    def hook(model, input, output):
-        activation[name] = output.detach()
+act = {}
+
+def get_activation1(name):
+    def hook1(module, input, output):
+        out = output.detach()
+        activation[name] = out
+    return hook1
+
+def get_activation(name): #only works with 4 GPUs
+    def hook(module, input, output):
+        batch_split = int(args.batch_size/4)
+        
+        out = output.detach()
+        
+        if str(output.device)[-1] == '0':
+            act[name][0:batch_split] = out
+        elif str(output.device)[-1] == '1':
+            act[name][batch_split:2*batch_split] = out
+        elif str(output.device)[-1] == '2':
+            act[name][2*batch_split:3*batch_split] = out
+        elif str(output.device)[-1] == '3':
+            act[name][3*batch_split:4*batch_split] = out
+
+#        print('In: ' + str(act[name].shape) + '  Out Device: ' + str(output.device)[-1])
     return hook
 
+def reg_hook1(model):
+    for name, module in model.module.named_modules():
+        if 'relu' in name or 'fc' in name:
+            module.register_forward_hook(get_activation1(name))
+        
 def reg_hook(model):
-    model.module.relu1.register_forward_hook(get_activation('relu1'))
-    model.module.relu2.register_forward_hook(get_activation('relu2'))
-    model.module.relu3.register_forward_hook(get_activation('relu3'))
-    model.module.relu4.register_forward_hook(get_activation('relu4'))
-    model.module.relu5.register_forward_hook(get_activation('relu5'))
-    model.module.relu6.register_forward_hook(get_activation('relu6'))
-    model.module.relu7.register_forward_hook(get_activation('relu7'))
-    model.module.relu8.register_forward_hook(get_activation('relu8'))
-    model.module.relu9.register_forward_hook(get_activation('relu9'))
-    model.module.relu10.register_forward_hook(get_activation('relu10'))
-    model.module.relu11.register_forward_hook(get_activation('relu11'))
-    model.module.relu12.register_forward_hook(get_activation('relu12'))
-    model.module.relu13.register_forward_hook(get_activation('relu13'))
-    model.module.relu14.register_forward_hook(get_activation('relu14'))
-    model.module.relu15.register_forward_hook(get_activation('relu15'))
-    model.module.relu16.register_forward_hook(get_activation('relu16'))
-    model.module.relu17.register_forward_hook(get_activation('relu17'))
-    model.module.relu18.register_forward_hook(get_activation('relu18'))
-    model.module.relu19.register_forward_hook(get_activation('relu19'))
-    model.module.relu19.register_forward_hook(get_activation('fc'))
+    for name, module in model.module.named_modules():
+        if 'relu' in name or 'fc' in name:
+            module.register_forward_hook(get_activation(name))
 
-def save_activations(batch_idx):
+def save_activations(model, batch_idx, act, labels):
     global act_path
-    torch.save(act_relu1, act_path+'/relu1/act_relu1_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu2, act_path+'/relu2/act_relu2_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu3, act_path+'/relu3/act_relu3_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu4, act_path+'/relu4/act_relu4_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu5, act_path+'/relu5/act_relu5_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu6, act_path+'/relu6/act_relu6_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu7, act_path+'/relu7/act_relu7_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu8, act_path+'/relu8/act_relu8_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu9, act_path+'/relu9/act_relu9_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu11, act_path+'/relu10/act_relu10_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu11, act_path+'/relu11/act_relu11_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu11, act_path+'/relu12/act_relu12_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu13, act_path+'/relu13/act_relu13_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu11, act_path+'/relu14/act_relu14_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu15, act_path+'/relu15/act_relu15_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu11, act_path+'/relu16/act_relu16_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu17, act_path+'/relu17/act_relu17_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu11, act_path+'/relu18/act_relu18_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_relu11, act_path+'/relu19/act_relu19_' +str(batch_idx) + '.pth.tar')
-    torch.save(act_fc, act_path+'/fc/act_fc_' +str(batch_idx) + '.pth.tar')
+    for name, module in model.module.named_modules():
+        if 'relu' in name or 'fc' in name:
+            torch.save(act[name], os.path.join(act_path, name) + '/act_' + name + '_' + str(batch_idx) + '.pth.tar')
     torch.save(labels, act_path+'/labels/labels_' +str(batch_idx) + '.pth.tar')
 
+class MyDataParallel(nn.DataParallel):
+    def __getattr(self, name):
+        return getattr(self.module, name)
+    
+#%%
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--batch-size', default=256, type=int,
-                         metavar='N', help='mini-batch size (default: 256)')
-    parser.add_argument('--dataset', metavar='DATASET', default='cifar100',
+    parser.add_argument('-b', '--batch-size', default=1000, type=int, metavar='N', 
+                help='mini-batch size (default: 256)')
+    parser.add_argument('--dataset', metavar='DATASET', default='cifar10',
                 help='dataset name or folder')
     parser.add_argument('--savedir', default='/home/nano01/a/esoufler/activations/multiple_batches/',
                 help='base path for saving activations')
     parser.add_argument('--model', '-a', metavar='MODEL', default='resnet20',
                 choices=model_names,
                 help='name of the model')
-    parser.add_argument('--pretrained', action='store', default=None,
-        help='the path to the pretrained model')
-    parser.add_argument('--mvm', action='store_true', default=None,
+    parser.add_argument('--pretrained', action='store', default='../pretrained_models/ideal/resnet20fp_cifar10.pth.tar',
+                help='the path to the pretrained model')
+    parser.add_argument('--mvm', action='store_true', default=False,
                 help='if running functional simulator backend')
-    parser.add_argument('--nideal', action='store_true', default=None,
+    parser.add_argument('--nideal', action='store_true', default=False,
                 help='Add xbar non-idealities')
-    parser.add_argument('--mode', default='test', help='save activations for \'train\' or \'test\' sets')
-    
+    parser.add_argument('--mode', default='test', 
+                help='save activations for \'train\' or \'test\' sets')
     parser.add_argument('--input_size', type=int, default=None,
                 help='image input size')
     parser.add_argument('-j', '--workers', default=8, type=int, metavar='J',
                 help='number of data loading workers (default: 8)')
-    parser.add_argument('--gpus', default='0', help='gpus (default: 4)')
+    parser.add_argument('--gpus', default='0,1,2,3', help='gpus (default: 4)')
     parser.add_argument('-exp', '--experiment', default='128x128', metavar='N',
                 help='experiment name')
-    
     args = parser.parse_args()
     
     print('\n' + ' '*6 + '==> Arguments:')
@@ -217,7 +215,7 @@ if __name__=='__main__':
     root_path = os.path.join(args.savedir, args.dataset, args.model)
     
     os.environ['CUDA_VISIBLE_DEVICES']= args.gpus
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('DEVICE:', device)
     print('GPU Id(s) being used:', args.gpus)
 
@@ -293,7 +291,7 @@ if __name__=='__main__':
         model = model_mvm
     
     model.to(device)#.half() # uncomment for FP16
-    model = torch.nn.DataParallel(model)
+    model = nn.DataParallel(model)
 
     default_transform = {
         'train': get_transform(args.dataset,
@@ -329,60 +327,30 @@ if __name__=='__main__':
     else:
         raise Exception('Invalid save mode')
         
-    os.makedirs(act_path + '/relu1', exist_ok=True)
-    os.makedirs(act_path + '/relu2', exist_ok=True)
-    os.makedirs(act_path + '/relu3', exist_ok=True)
-    os.makedirs(act_path + '/relu4', exist_ok=True)
-    os.makedirs(act_path + '/relu5', exist_ok=True)
-    os.makedirs(act_path + '/relu6', exist_ok=True)
-    os.makedirs(act_path + '/relu7', exist_ok=True)
-    os.makedirs(act_path + '/relu8', exist_ok=True)
-    os.makedirs(act_path + '/relu9', exist_ok=True)
-    os.makedirs(act_path + '/relu10', exist_ok=True)
-    os.makedirs(act_path + '/relu11', exist_ok=True)
-    os.makedirs(act_path + '/relu12', exist_ok=True)
-    os.makedirs(act_path + '/relu13', exist_ok=True)
-    os.makedirs(act_path + '/relu14', exist_ok=True)
-    os.makedirs(act_path + '/relu15', exist_ok=True)
-    os.makedirs(act_path + '/relu16', exist_ok=True)
-    os.makedirs(act_path + '/relu17', exist_ok=True)
-    os.makedirs(act_path + '/relu18', exist_ok=True)
-    os.makedirs(act_path + '/relu19', exist_ok=True)
+    for i in range(1, 20):
+        os.makedirs(act_path + '/relu' + str(i), exist_ok=True)
     os.makedirs(act_path + '/fc', exist_ok=True)
     os.makedirs(act_path + '/labels', exist_ok=True)
     
     
     print("Saving activations to: {}".format(act_path))
     for batch_idx,(data, target) in enumerate(dataloader):
-        reg_hook(model)
+        reg_hook1(model)
         target = target.to(device)
         data_var = torch.autograd.Variable(data.to(device))
         target_var = torch.autograd.Variable(target.to(device))
         print('Dry run for computing activation sizes...')
         output = model(data_var)
+#        print(activation['relu1'].shape())
+        print('Dry run finished...')
         
         break
-    
-    act_relu1 = torch.zeros([args.batch_size, activation['relu1'].shape[1], activation['relu1'].shape[2], activation['relu1'].shape[3]])
-    act_relu2 = torch.zeros([args.batch_size, activation['relu2'].shape[1], activation['relu2'].shape[2], activation['relu2'].shape[3]])
-    act_relu3 = torch.zeros([args.batch_size, activation['relu3'].shape[1], activation['relu3'].shape[2], activation['relu3'].shape[3]])
-    act_relu4 = torch.zeros([args.batch_size, activation['relu4'].shape[1], activation['relu4'].shape[2], activation['relu4'].shape[3]])
-    act_relu5 = torch.zeros([args.batch_size, activation['relu5'].shape[1], activation['relu5'].shape[2], activation['relu5'].shape[3]])
-    act_relu6 = torch.zeros([args.batch_size, activation['relu6'].shape[1], activation['relu6'].shape[2], activation['relu6'].shape[3]])
-    act_relu7 = torch.zeros([args.batch_size, activation['relu7'].shape[1], activation['relu7'].shape[2], activation['relu7'].shape[3]])
-    act_relu8 = torch.zeros([args.batch_size, activation['relu8'].shape[1], activation['relu8'].shape[2], activation['relu8'].shape[3]])
-    act_relu9 = torch.zeros([args.batch_size, activation['relu9'].shape[1], activation['relu9'].shape[2], activation['relu9'].shape[3]])
-    act_relu10 = torch.zeros([args.batch_size, activation['relu10'].shape[1], activation['relu10'].shape[2], activation['relu10'].shape[3]])
-    act_relu11 = torch.zeros([args.batch_size, activation['relu11'].shape[1], activation['relu11'].shape[2], activation['relu11'].shape[3]])
-    act_relu12 = torch.zeros([args.batch_size, activation['relu12'].shape[1], activation['relu12'].shape[2], activation['relu12'].shape[3]])
-    act_relu13 = torch.zeros([args.batch_size, activation['relu13'].shape[1], activation['relu13'].shape[2], activation['relu13'].shape[3]])
-    act_relu14 = torch.zeros([args.batch_size, activation['relu14'].shape[1], activation['relu14'].shape[2], activation['relu14'].shape[3]])
-    act_relu15 = torch.zeros([args.batch_size, activation['relu15'].shape[1], activation['relu15'].shape[2], activation['relu15'].shape[3]])
-    act_relu16 = torch.zeros([args.batch_size, activation['relu16'].shape[1], activation['relu16'].shape[2], activation['relu16'].shape[3]])
-    act_relu17 = torch.zeros([args.batch_size, activation['relu17'].shape[1], activation['relu17'].shape[2], activation['relu17'].shape[3]])
-    act_relu18 = torch.zeros([args.batch_size, activation['relu18'].shape[1], activation['relu18'].shape[2], activation['relu18'].shape[3]])
-    act_relu19 = torch.zeros([args.batch_size, activation['relu19'].shape[1], activation['relu19'].shape[2], activation['relu19'].shape[3]])
-    act_fc = torch.zeros([args.batch_size, activation['fc'].shape[1], activation['relu17'].shape[2], activation['relu17'].shape[3]])
+    act = {}
+    for name, module in model.module.named_modules():
+        if 'relu' in name:
+            act[name] = torch.zeros([args.batch_size, activation[name].shape[1], activation[name].shape[2], activation[name].shape[3]])
+        if 'fc' in name:
+            act[name] = torch.zeros([args.batch_size, activation[name].shape[1]])
     labels = torch.zeros([args.batch_size])
 
     for batch_idx,(data, target) in enumerate(dataloader):
@@ -394,30 +362,12 @@ if __name__=='__main__':
     
         output = model(data_var)
         
-        act_relu1 = activation['relu1']
-        act_relu2 = activation['relu2']
-        act_relu3 = activation['relu3']
-        act_relu4= activation['relu4']
-        act_relu5 = activation['relu5']
-        act_relu6 = activation['relu6']
-        act_relu7 = activation['relu7']
-        act_relu8 = activation['relu8']
-        act_relu9 = activation['relu9']
-        act_relu10 = activation['relu10']
-        act_relu11 = activation['relu11']
-        act_relu12 = activation['relu12']
-        act_relu13 = activation['relu13']
-        act_relu14 = activation['relu14']
-        act_relu15 = activation['relu15']
-        act_relu16 = activation['relu16']
-        act_relu17 = activation['relu17']
-        act_relu18 = activation['relu18']
-        act_relu19 = activation['relu19']
-        act_fc = activation['fc']
+#        for name, module in model.module.named_modules():
+#            if 'relu' in name or 'fc' in name:
+#                print('Out: ' + str(act[name].shape))
         labels = target
-        
+
+        save_activations(model=model, batch_idx=batch_idx, act=act, labels=labels)
         duration = time.time() - base_time
-#        print("Batch IDx: {}".format(batch_idx))
-        print("Batch IDx: {} \t Time taken: {}m {}secs".format(batch_idx, int(duration)//60, int(duration)%60))    
-        save_activations(batch_idx=batch_idx)
+        print("Batch IDx: {} \t Time taken: {}m {}secs".format(batch_idx, int(duration)//60, int(duration)%60))
     
